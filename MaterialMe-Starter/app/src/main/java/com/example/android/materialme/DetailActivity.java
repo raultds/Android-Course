f78faca6c9ed763093ee688d3647261ad049d9e8
Roca Junca <rlrc@espc011721.lan>
2020-04-08 08:14:35 +0200

    finish unit 2

---
+++ MaterialMe-Starter/app/src/main/java/com/example/android/materialme/DetailActivity.java
@@ -0,0 +1,28 @@
+package com.example.android.materialme;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+
+        // Initialize the views.
+        TextView sportsTitle = findViewById(R.id.titleDetail);
+        ImageView sportsImage = findViewById(R.id.sportsImageDetail);
+
+        // Set the text from the Intent extra.
+        sportsTitle.setText(getIntent().getStringExtra("title"));
+
+        // Load the image using the Glide library and the Intent extra.
+        Glide.with(this).load(getIntent().getIntExtra("image_resource",0))
+                .into(sportsImage);
+    }
+}
