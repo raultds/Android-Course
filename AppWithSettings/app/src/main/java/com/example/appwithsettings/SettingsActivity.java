441dcb6d1b5218423fa060d74302e36e06d3f660
Roca Junca <rlrc@espc011721.lan>
2020-04-09 09:36:00 +0200

    notifications and shared preferences

---
+++ AppWithSettings/app/src/main/java/com/example/appwithsettings/SettingsActivity.java
@@ -0,0 +1,12 @@
+package com.example.appwithsettings;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ AppWithSettings/app/src/main/java/com/example/appwithsettings/SettingsFragment.java
@@ -0,0 +1,28 @@
+package com.example.appwithsettings;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+}
