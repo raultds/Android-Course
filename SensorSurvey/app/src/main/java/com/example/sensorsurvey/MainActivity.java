a48fda25736462b837b9facfd121150e396c62be
Roca Junca <rlrc@espc011721.lan>
2020-04-14 14:40:10 +0200

    sensors

---
+++ SensorSurvey/app/src/main/java/com/example/sensorsurvey/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.sensorsurvey;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    // Individual light and proximity sensors.
+    private Sensor mSensorProximity;
+    private Sensor mSensorLight;
+
+    // TextViews to display current sensor values
+    private TextView mTextSensorLight;
+    private TextView mTextSensorProximity;
+
+    private SensorManager sensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        mTextSensorLight = findViewById(R.id.label_light);
+        mTextSensorProximity = findViewById(R.id.label_proximity);
+
+        mSensorProximity = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mSensorLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        String sensor_error = getResources().getString(R.string.error_no_sensor);
+
+        if (mSensorLight == null) {
+            mTextSensorLight.setText(sensor_error);
+        }
+        if (mSensorProximity == null) {
+            mTextSensorProximity.setText(sensor_error);
+        }
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if(mSensorProximity != null){
+            sensorManager.registerListener(this, mSensorProximity,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mSensorLight != null) {
+            sensorManager.registerListener(this, mSensorLight,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int sensorType = event.sensor.getType();
+        float currentValue = event.values[0];
+        switch(sensorType){
+            case Sensor.TYPE_LIGHT:
+                mTextSensorLight.setText(getResources().getString(
+                        R.string.label_light, currentValue));
+                break;
+            case Sensor.TYPE_PROXIMITY:
+                mTextSensorProximity.setText(getResources().getString(
+                        R.string.label_proximity, currentValue));
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
